4e6b8045f301f2d46a7b3577c168fd942d657017
Vladimir Sviridenko <vladimirsviridenko1998@gmail.com>
2020-06-04 01:28:08 +0300

    feat: add options

--- src/app/_core/services/products-options.service.ts
+++ src/app/_core/services/products-options.service.ts
@@ -65,7 +65,108 @@ export class ProductsOptionsService {
             this.skinFabric('5095', 'Роялвуд графит', 2),
             this.skinFabric('54', 'Патина премиум', 2),
             this.skinFabric('5457', 'Патина ясень серебро', 2),
-            this.skinFabric('85', 'Патина ясень золото', 2)
+            this.skinFabric('85', 'Патина ясень золото', 2),
+            this.skinFabric('7539', 'Патина фисташка', 3),
+            this.skinFabric('246TP', 'Зелень патина', 2),
+            this.skinFabric('74', 'Патина скай', 2),
+            this.skinFabric('7534', 'Патина дуб бирюза', 3),
+            this.skinFabric('166', 'Дуб терракот', 2),
+            this.skinFabric('165', 'Дуб болотный', 2),
+            this.skinFabric('162', 'Дуб аквамарин', 2),
+            this.skinFabric('160', 'Дуб адриатика', 2),
+            this.skinFabric('5018', 'Роялвуд голубой', 2),
+            this.skinFabric('5094', 'Роялвуд джинс', 2),
+          ]
+        },
+        {
+          name: 'Древесные глянцевые',
+          options: [
+            this.skinFabric('21HG', 'Зербано белый', 4),
+            this.skinFabric('6HG', 'Дуб золото светлый', 4),
+            this.skinFabric('241G', 'Зебрано', 3),
+            this.skinFabric('28HG', 'Дуб золото тёмный', 4)
+          ]
+        },
+        {
+          name: 'Однотонные глянцевые',
+          options: [
+            this.skinFabric('6G', 'Белый', 2),
+            this.skinFabric('2126', 'Крем глянец', 2),
+            this.skinFabric('3041PG', 'Крем брюлле', 4),
+            this.skinFabric('35G', 'Ваниль глянец', 2),
+            this.skinFabric('16G', 'Лимон', 2),
+            this.skinFabric('1495G', 'Оранжевый', 2),
+            this.skinFabric('511G', 'Капучино', 2),
+            this.skinFabric('283G', 'Красный', 2),
+            this.skinFabric('641G', 'Бордо', 2),
+            this.skinFabric('803G', 'Лайм', 3),
+            this.skinFabric('333G', 'Виолетта', 2),
+            this.skinFabric('515G', 'Баклажан', 2),
+            this.skinFabric('7740', 'Шоколад глянец', 2),
+            this.skinFabric('2905', 'Чёрный', 2),
+            this.skinFabric('18003', 'Сирень глянец', 3),
+            this.skinFabric('2129G', 'Графит', 2),
+            this.skinFabric('535-6TG', 'Какао пурламутр', 2),
+            this.skinFabric('1200G', 'Белый (Тёплый)', 2)
+          ]
+        },
+        {
+          name: 'Однотонные матовые',
+          options: [
+            this.skinFabric('93005', 'Белый софт', 2),
+            this.skinFabric('14027', 'Белый матовый', 1),
+            this.skinFabric('35', 'Ваниль', 1),
+            this.skinFabric('1495', 'Оранжевый матовый', 1),
+            this.skinFabric('1291', 'Эвкалипт', 1),
+            this.skinFabric('109TP', 'Синий', 1),
+            this.skinFabric('8916', 'Супермат панакота', 2),
+            this.skinFabric('8925', 'Супермат фисташка', 2),
+            this.skinFabric('8913', 'Супермат оливка', 2),
+            this.skinFabric('8912', 'Супермат шардоне', 2),
+            this.skinFabric('7029', 'Шоколад софт', 2),
+            this.skinFabric('013', 'Аквамарин', 2),
+            this.skinFabric('015', 'Ниагара', 2),
+            this.skinFabric('018', 'Фиалка', 2),
+            this.skinFabric('854', 'Латте', 2),
+            this.skinFabric('855', 'Грей', 2),
+            this.skinFabric('856', 'Графит', 2)
+          ]
+        },
+        {
+          name: 'Фантазийные матовые',
+          options: [
+            this.skinFabric('M06', 'Камень коричневый', 3),
+            this.skinFabric('M07', 'Камень светло-серый', 3),
+          ]
+        },
+        {
+          name: 'Металики',
+          options: [
+            this.skinFabric('125HG', 'Белый', 3),
+            this.skinFabric('2106HG', 'Ваниль металлик', 3),
+            this.skinFabric('9519HG', 'Кремовый', 3),
+            this.skinFabric('805HG', 'Лимон', 3),
+            this.skinFabric('303HG', 'Шампань светлая', 3),
+            this.skinFabric('2113HG', 'Капучино металлик', 3),
+            this.skinFabric('501HG', 'Шампань', 3),
+            this.skinFabric('2133HG', 'Шоколад металлик', 3),
+            this.skinFabric('151CHG', 'Розовая сирень', 3),
+            this.skinFabric('9518HG', 'Абрикос', 4),
+            this.skinFabric('264HG', 'Оранжевый', 3),
+            this.skinFabric('18HG', 'Апельсин', 4),
+            this.skinFabric('401HG', 'Красный', 3),
+            this.skinFabric('400HG', 'Фиалка', 3),
+            this.skinFabric('857HG', 'Фиолетовый', 3),
+            this.skinFabric('2119HG', 'Малина металлик', 3),
+            this.skinFabric('404HG', 'Корица', 3),
+            this.skinFabric('903HG', 'Пурпур', 3),
+            this.skinFabric('308HG', 'Голубой', 3),
+            this.skinFabric('067HG', 'Бирюза', 3),
+            this.skinFabric('760HG', 'Морская волна', 4),
+            this.skinFabric('802-6THG', 'Синий', 3),
+            this.skinFabric('666HG', 'Серебро тёмное', 3),
+            this.skinFabric('757HG', 'Фисташковый', 3),
+            this.skinFabric('865HG', 'Яблоко', 4)
           ]
         }
       ]
