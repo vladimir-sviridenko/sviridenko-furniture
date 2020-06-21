f301278b594356acb623aed04aa2428b016214cd
Vladimir Sviridenko <vladimirsviridenko1998@gmail.com>
2020-06-21 17:17:11 +0300

    feat: add styles for home-page

--- src/app/components/shop/components/header/header.component.ts
+++ src/app/components/shop/components/header/header.component.ts
@@ -1,12 +1,7 @@
 import { Component, ChangeDetectionStrategy, AfterViewInit } from '@angular/core';
-import { MatDialog, MatDialogRef } from '@angular/material/dialog';
-import { ContactsComponent } from '../contacts/contacts.component';
-import { Overlay } from '@angular/cdk/overlay';
 import { FocusMonitor } from '@angular/cdk/a11y';
-import {BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
-import { last, take } from 'rxjs/operators';
-import { Observable } from 'rxjs';
 import { DialogService } from '@shop/services/dialog.service';
+import { ShopFacadeService } from '@store/facades/shop.facade';
 
 @Component({
 	selector: 'app-header',
@@ -17,7 +12,8 @@ import { DialogService } from '@shop/services/dialog.service';
 export class HeaderComponent implements AfterViewInit {
 
 	constructor(public dialogService: DialogService,
-							private focusMonitor: FocusMonitor) { }
+							private focusMonitor: FocusMonitor,
+							public shopFacadeService: ShopFacadeService) { }
 
 	public ngAfterViewInit(): void {
 		this.focusMonitor.stopMonitoring(document.querySelector('.header__contacts-switcher'));
